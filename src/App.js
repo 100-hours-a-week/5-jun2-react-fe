36bce25f6577855a31f2074d9cfe08ef6212468e
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-05-09 01:30:14 +0900

    chore: 프로젝트 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React from "react";
+import { Reset } from "styled-reset";
+
 function App() {
-  return <div className="App">Initial Project</div>;
+  return (
+    <div className="App">
+      <React.Fragment>
+        <Reset />
+        Initial Project
+      </React.Fragment>
+    </div>
+  );
 }
 
 export default App;
