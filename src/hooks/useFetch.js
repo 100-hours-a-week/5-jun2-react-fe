6a294a668019ed6fd539382b98a6fd442a65552a
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-05-12 21:39:00 +0900

    feat: useFetch 커스텀 훅 구현

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+
+  useEffect(function getBoardList() {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return data;
+};
+
+export default useFetch;
