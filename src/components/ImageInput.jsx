480a159e30e731b5d688de3c355810c0bd118be3
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-05-14 17:42:13 +0900

    feat: 프로필 이미지 업로드 기능 구현

--- src/components/ImageInput.jsx
+++ src/components/ImageInput.jsx
@@ -1,6 +1,36 @@
 import style from '../styles/components/ImageInput.module.css';
+import { useRef, useState } from 'react';
 
-const ImageInput = ({ title, crossLine, imageUrl, helperText }) => {
+const ImageInput = ({ title, imageUrl, helperText }) => {
+  const [profileImg, setProfileImg] = useState([]);
+  const [crossLine, setCrossLine] = useState(true);
+  const inputRef = useRef(null);
+
+  const onClickHandler = () => {
+    console.log(inputRef.current);
+    inputRef.current.click();
+  };
+
+  const uploadProfileHandler = (e) => {
+    const selectFile = e.target.files[0];
+    if (selectFile) {
+      setProfileImg(selectFile);
+      setCrossLine(false);
+    } else {
+      setProfileImg(imageUrl);
+      setCrossLine(true);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setProfileImg(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
   return (
     <div className={style.imageInputWrapper}>
       <div className={style.textBox}>
@@ -8,10 +38,16 @@ const ImageInput = ({ title, crossLine, imageUrl, helperText }) => {
         <p className={style.helperText}>{helperText}</p>
       </div>
       <div>
-        <input type="file" accept="image/*" multiple className={style.imageInput}></input>
-        <div className={style.circleBox}>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={uploadProfileHandler}
+          className={style.imageInput}
+          ref={inputRef}
+        ></input>
+        <div onClick={onClickHandler} className={style.circleBox}>
           {crossLine && <div className={style.crossLine}></div>}
-          <img className={style.profileImg} src={imageUrl} alt={'hi'}></img>
+          <img className={style.profileImg} src={profileImg ? profileImg : imageUrl}></img>
         </div>
       </div>
     </div>
